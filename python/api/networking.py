f6372566281c3f255e2853440c2b5385f2910e55
Rodolfo Martínez Vega <rodomar@outlook.com>
2023-03-08 16:48:35 -0600

    Complete networking methods

--- python/api/networking.py
+++ python/api/networking.py
@@ -1,21 +1,35 @@
-""" Business logic. """
-#import re
+""" Classes for networking module. """
+from socket import inet_ntoa
+from struct import pack
+from ipaddress import ip_address
 
 class CidrMaskConvert:
     """ Convertion between CIDR and mask. """
-    def cidr_to_mask(self, val):
+    def cidr_to_mask(self, cidr):
         """" Convert CIDR to mask. """
-        print(val)
-        return True
+        host_bits = 32 - int(cidr)
+        mask = inet_ntoa(pack("!I", (1 << 32) - (1 << host_bits)))
 
-    def mask_to_cidr(self, val):
+        if mask == "0.0.0.0":
+            return "Invalid"
+
+        return mask
+
+    def mask_to_cidr(self, mask):
         """" Convert mask to CIDR. """
-        print(val)
-        return True
+        cidr = str(sum(bin(int(x)).count("1") for x in mask.split(".")))
+
+        if cidr == "0":
+            return "Invalid"
+
+        return cidr
 
 class IpValidate:
     """ IP Validation. """
-    def ipv4_validation(self, val):
-        """ Validate IPV4. """
-        print(val)
-        return True
+    def ipv4_validation(self, address):
+        """ Validate IPV4 address. """
+        try:
+            ip_address(address)
+            return True
+        except ValueError:
+            return False
