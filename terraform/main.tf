e28c53200ff8f3484da4a6cae349991363f7f454
Rodolfo Martínez Vega <rodomar@outlook.com>
2023-03-27 16:57:10 -0600

    Fix main.tf by runnint terraform fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,11 @@
 locals {
-  account_id  = data.aws_caller_identity.current.account_id
-  region      = "us-west-2"
-  env         = "dev"
-  app_name    = "capstoneAPItf"
+  account_id = data.aws_caller_identity.current.account_id
+  region     = "us-west-2"
+  env        = "dev"
+  app_name   = "capstoneAPItf"
 
   default_tags = {
-    env         = local.env
+    env = local.env
   }
 
   ecs_tags = merge(local.default_tags, {
@@ -16,10 +16,10 @@ locals {
   ecr_repository = "capstone-api"
   ecr_image_tag  = "deliver-final"
 
-  service_port          = 80
-  protocol              = "HTTP"
-  subnets               = ["subnet-34c72d4c", "subnet-fa4156b1", "subnet-0d4f8a50", "subnet-be295595"]
-  vpc_id                = "vpc-b272ebca"
+  service_port = 80
+  protocol     = "HTTP"
+  subnets      = ["subnet-34c72d4c", "subnet-fa4156b1", "subnet-0d4f8a50", "subnet-be295595"]
+  vpc_id       = "vpc-b272ebca"
 }
 
 module "ecs" {
